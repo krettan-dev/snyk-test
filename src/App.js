7648cd50d2d62a651fc61c52f722543328c7ead3
Carl Retzner <retzner@calles-thinkpad.localdomain>
2022-11-15 20:55:16 +0100

    Update logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import logo from './cshift-circle.svg';
+import cshiftC from './cshift-c.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <img src={cshiftC} className="App-logo-c" alt="c" />
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
