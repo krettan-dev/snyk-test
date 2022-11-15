11630f6ce6ace36b3f9ad0f005ddf6b48c392744
Carl Retzner <retzner@calles-thinkpad.localdomain>
2022-11-15 20:57:22 +0100

    Remove some text and update color

--- src/App.js
+++ src/App.js
@@ -10,17 +10,6 @@ function App() {
           <img src={cshiftC} className="App-logo-c" alt="c" />
           <img src={logo} className="App-logo" alt="logo" />
         </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
